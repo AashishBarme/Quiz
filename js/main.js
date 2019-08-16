15780de15e76aede58dda59185f92ec71fa1b7d8
Aashish Barme <aashish.barme9@gmail.com>
2019-08-16 20:20:28 +0545

    Quiz in Javascript

---
+++ js/main.js
@@ -0,0 +1,84 @@
+
+let quiz = [	{
+ 		"question":"What is 1 + 1 ?",
+ 		"option":[
+ 					4,
+ 					2,
+ 					7,
+ 					0
+ 				 ],
+ 		"answer":2
+	},
+	{
+ 		"question":"What is 4 - 0 ?",
+ 		"option":[
+ 					4,
+ 					5,
+ 					1,
+ 					0
+ 				 ],
+ 		"answer":4
+	}]
+let count = 0;
+for (i=0;i<quiz.length;i++){
+	let quizOuter = document.getElementById('quiz');
+	let queOuter = document.createElement('div');
+	queOuter.setAttribute('class','que-ans');
+	queOuter.setAttribute('id','que-ans'+i);
+
+	let zQuestion = document.createElement('div');
+	zQuestion.setAttribute('id','question'+i);
+	zQuestion.innerHTML = quiz[i].question;
+	quizOuter.appendChild(queOuter);
+	queOuter.appendChild(zQuestion);
+
+
+for (j=0;j<4;j++){
+	//Create a input type radio option 
+	let zOption = document.createElement('input');
+	zOption.setAttribute('type','radio');
+	zOption.setAttribute('id','option'+i+j);
+	zOption.setAttribute('name','select'+i);
+	zOption.setAttribute('value', quiz[i].option[j]);
+	//create a label in input
+	let zLabel = document.createElement('label');
+	zLabel.innerHTML = quiz[i].option[j];
+	queOuter.appendChild(zOption);
+	queOuter.appendChild(zLabel);
+
+
+}
+// console.log(quizOuter.children);
+}
+
+
+
+
+
+function myPoint(){
+	let count = 0;
+	for (i = 0 ; i <quiz.length ; i++){
+		for (j = 0 ; j <4 ; j++){
+			let userAnswer = document.getElementById('option'+i+j);
+			if(userAnswer.checked){
+	 	if(userAnswer.value == quiz[i].answer){
+	 		count  = count + 1;
+	 		document.getElementById('que-ans'+i).style.border ='1px solid green';
+	 		document.getElementById('que-ans'+i).style.boxShadow ='2px 2px green';
+	 			} else {
+			document.getElementById('que-ans'+i).style.border ='1px solid red';
+	 		document.getElementById('que-ans'+i).style.boxShadow ='2px 2px red';
+	 			}
+	 		}
+	 	}
+	}	
+	 	document.getElementById('mark').innerHTML = count;	
+	 	let checker  = quiz.length / 2;
+	 	if (count >= checker ){
+		document.getElementById('gif-welldone').style.display ='block';
+		document.getElementById('gif-bad').style.display ='none';
+		} else {
+		document.getElementById('gif-bad').style.display ='block';	
+		document.getElementById('gif-welldone').style.display ='none';
+		}
+	}
